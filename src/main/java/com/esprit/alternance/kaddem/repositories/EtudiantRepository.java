bbcfb366665ac847eb10f6ea50de7e8a94a06027
wejdene bedoui <wejdene.bedoui98@gmail.com>
2023-02-14 21:30:44 +0100

    add request in EtudiantRepository

--- src/main/java/com/esprit/alternance/kaddem/repositories/EtudiantRepository.java
+++ src/main/java/com/esprit/alternance/kaddem/repositories/EtudiantRepository.java
@@ -3,5 +3,8 @@ package com.esprit.alternance.kaddem.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class EtudiantRepository  extends JpaRepository<Etudiant, Long> {
-    
+
+    public List<Etudiant> findByDepartement(Departement departement);
+
+    public List<Etudiant> findByEquipeNiveau(Niveau niveau);
 }
